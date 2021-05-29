ef98f941f29f984aabed58b267413ff2d7cce6f4
MadhavPanicker <49715830+MadhavPanicker@users.noreply.github.com>
2021-05-29 22:18:55 +0530

    Add files via upload

---
+++ VCode/blog2.php
@@ -0,0 +1,39 @@
+<div class="instr">
+        <div class="instra">
+            <br>
+            <h1 style="text-align: center;">Blog:</h1><br><br>
+            <div class="user1">
+                <img src="https://static.toiimg.com/photo/msid-75805310/75805310.jpg?135092" alt="Robert Downey Jr."
+                    id="user1">
+                <p style="color:white;">"I always felt that not learning web development had been my biggest
+                    disappointment, apart from my untimely
+                    death in the Endgame. Hence, I had taken up a course in VCode. I was surprised to see the experience
+                    of top faculties teaching through
+                    this portal. Now I feel as confident in web development as the time when I rode through space."</p>
+                <br>
+                <p style="text-align: right; color: white;">-Robert Downey Jr.</p>
+            </div><br>
+            <div class="user2">
+
+                <img src="https://www.giantfreakinrobot.com/wp-content/uploads/2021/02/chris-pratt.jpg"
+                    alt="Chris Pratt" id="user2">
+                <p style="color: white;">"I am not *that good with technology, to be honest. I wanted to catch up with
+                    the latest fad.
+                    Hence, I saw this wonderful opportunity of learning web development in VCode. I was extreamly
+                    pleased with the faculty's
+                    expertise in the field. I passed the examination with flying colors. Now, I am a certified web
+                    developer. All thanks to
+                    VCode and the faculty team."</p><br>
+                <p style="text-align: right; color: white;">-Chris Pratt</p>
+            </div><br>
+            <div class="user3">
+                <img src="https://cdn.britannica.com/61/137461-050-BB6C5D80/Brad-Pitt-2008.jpg" alt="Brad Pitt"
+                    id="user3">
+                <p style="color: white;">My coding skills were weak. VCode helped me to develop them and now I feel
+                    confident
+                    again!!!</p><br>
+                <p style="text-align: right; color: white;">-Brad Pitt</p>
+            </div><br>
+        </div>
+    </div>
+    <br><br><br>
